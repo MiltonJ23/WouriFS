877c7212a5d94429451e58c8d3542799137733ed
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2026-04-25 08:28:39 +0000

    feat: implemented the TokenManager contracts for Generating token and verifying them

---
+++ internal/auth/jwt/manager.go
@@ -0,0 +1,74 @@
+package jwt
+
+import (
+	"context"
+	"crypto/rsa"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/MiltonJ23/WouriFS/internal/domain"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type RSATokenManager struct {
+	PrivateKey *rsa.PrivateKey
+	PublicKey  *rsa.PublicKey
+}
+
+type CustomClaims struct {
+	Username  string `json:"username"`
+	Namespace string `json:"namespace"`
+	jwt.RegisteredClaims
+}
+
+func NewRSATokenManager(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey) *RSATokenManager {
+	return &RSATokenManager{
+		PrivateKey: privateKey,
+		PublicKey:  publicKey,
+	}
+}
+
+func (m *RSATokenManager) GenerateToken(ctx context.Context, user *domain.User, duration time.Duration) (string, error) {
+	now := time.Now()
+	claims := CustomClaims{
+		Username:  user.Username,
+		Namespace: user.Namespace,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   user.Username,
+			ExpiresAt: jwt.NewNumericDate(now),
+			IssuedAt:  jwt.NewNumericDate(now.Add(duration)),
+			Issuer:    "wourifs-auth",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	return token.SignedString(m.PrivateKey)
+}
+
+func (m *RSATokenManager) VerifyToken(ctx context.Context, tokenStr string) (*domain.TokenPayload, error) {
+	token, parseTokenWithClaimsErr := jwt.ParseWithClaims(tokenStr, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, errors.New("security violation: invalid signing method")
+		}
+		return m.PublicKey, nil
+	})
+
+	if parseTokenWithClaimsErr != nil {
+		return nil, fmt.Errorf("an error occurred parsing token: %w", parseTokenWithClaimsErr)
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token claims ")
+	}
+
+	return &domain.TokenPayload{
+		UserID:    claims.Subject,
+		Username:  claims.Username,
+		Namespace: claims.Namespace,
+		IssuedAt:  claims.IssuedAt.Time,
+		ExpiredAt: claims.ExpiresAt.Time,
+	}, nil
+}
