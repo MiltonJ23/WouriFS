d698f4d64ffcacdb4d7ca556f6aa338cc5ac1460
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2026-04-27 07:56:32 +0000

    feat: added the InMemoryDataNpdeRegistry and implemented all of the DataNodeRegistry Interface

---
+++ internal/domain/namenode/memory_registry.go
@@ -0,0 +1,96 @@
+package namenode
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var (
+	ErrNodeNotFound = errors.New("datanode not found in registry")
+	ErrInvalidNode  = errors.New("invalid datanode data")
+)
+
+type InMemoryDataNodeRegistry struct {
+	mu    sync.RWMutex
+	nodes map[string]*DataNodeStatus
+}
+
+func NewInMemoryDataNodeRegistry() *InMemoryDataNodeRegistry {
+	return &InMemoryDataNodeRegistry{
+		nodes: make(map[string]*DataNodeStatus),
+	}
+}
+
+func (i *InMemoryDataNodeRegistry) Register(node *DataNodeStatus) error {
+	if node == nil || node.ID == "" || node.Address == "" {
+		return ErrInvalidNode
+	}
+
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	node.LastHeartbeat = time.Now()
+	node.IsAvailable = true
+	i.nodes[node.ID] = node
+
+	return nil
+}
+
+func (i *InMemoryDataNodeRegistry) UpdateHeartbeat(id string, freeStorage int64, activeConnections int32) error {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	node, exists := i.nodes[id]
+	if !exists {
+		return ErrNodeNotFound
+	}
+	node.FreeStorageBytes = freeStorage
+	node.ActiveConnection = activeConnections
+	node.LastHeartbeat = time.Now()
+
+	node.IsAvailable = true
+
+	return nil
+}
+
+func (i *InMemoryDataNodeRegistry) MarkUnavailable(id string) error {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	node, exists := i.nodes[id]
+	if !exists {
+		return ErrNodeNotFound
+	}
+
+	node.IsAvailable = false
+	return nil
+}
+
+func (i *InMemoryDataNodeRegistry) GetAllAvailable() []*DataNodeStatus {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	var availableNodes []*DataNodeStatus
+
+	for _, node := range i.nodes {
+		if !node.IsAvailable {
+			nodeCopy := *node
+			availableNodes = append(availableNodes, &nodeCopy)
+		}
+	}
+
+	return availableNodes
+}
+
+func (i *InMemoryDataNodeRegistry) GetAll() []*DataNodeStatus {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	var allNodes []*DataNodeStatus
+	for _, node := range i.nodes {
+		nodeCopy := *node
+		allNodes = append(allNodes, &nodeCopy)
+	}
+	return allNodes
+}
