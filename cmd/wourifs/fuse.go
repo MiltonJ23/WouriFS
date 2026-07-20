d22141c7abe0bc83563942849830463e168d11e9
MiltonJ23 <jonhmilton1991@gmail.com>
2026-07-20 11:55:33 +0100

    fix: add gRPC error mapping, context deadlines, connection pooling to FUSE operations

--- cmd/wourifs/fuse.go
+++ cmd/wourifs/fuse.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"sync"
 	"syscall"
+	"time"
 
 	pb "github.com/MiltonJ23/WouriFS/api/gen/v1/namenode"
 	datanodepb "github.com/MiltonJ23/WouriFS/api/gen/v1/datanode"
@@ -11,11 +14,47 @@ import (
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
-// --- FUSE inode ---
+const rpcTimeout = 10 * time.Second
 
+// mapGRPCErr translates a gRPC error into a syscall errno for FUSE.
+// If err is not a gRPC status error, fallback is returned.
+func mapGRPCErr(err error, fallback syscall.Errno) syscall.Errno {
+	if err == nil {
+		return 0
+	}
+	st, ok := status.FromError(err)
+	if !ok {
+		return fallback
+	}
+	switch st.Code() {
+	case codes.NotFound:
+		return syscall.ENOENT
+	case codes.PermissionDenied:
+		return syscall.EACCES
+	case codes.AlreadyExists:
+		return syscall.EEXIST
+	case codes.Unavailable:
+		return syscall.EAGAIN
+	default:
+		return fallback
+	}
+}
+
+// dialNN creates a gRPC connection. Callers should reuse the connection.
+func dialNN(addr string) (*grpc.ClientConn, error) {
+	return grpc.NewClient(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(64<<20)),
+	)
+}
+
+// wourifsNode is the FUSE root inode backed by WouriFS gRPC.
+// It holds a long-lived Namenode connection reused across operations.
 type wourifsNode struct {
 	fs.Inode
 	path   string
@@ -24,18 +63,33 @@ type wourifsNode struct {
 	mu     sync.Mutex
 }
 
-var _ fs.NodeOnAdder = (*wourifsNode)(nil)
+// Compile-time interface checks.
+var (
+	_ fs.NodeOnAdder   = (*wourifsNode)(nil)
+	_ fs.NodeLookuper  = (*wourifsNode)(nil)
+	_ fs.NodeGetattrer = (*wourifsNode)(nil)
+	_ fs.NodeSetattrer = (*wourifsNode)(nil)
+	_ fs.NodeOpener    = (*wourifsNode)(nil)
+	_ fs.NodeCreater   = (*wourifsNode)(nil)
+	_ fs.NodeMkdirer   = (*wourifsNode)(nil)
+	_ fs.NodeRmdirer   = (*wourifsNode)(nil)
+	_ fs.NodeUnlinker  = (*wourifsNode)(nil)
+	_ fs.NodeRenamer   = (*wourifsNode)(nil)
+	_ fs.NodeReaddirer = (*wourifsNode)(nil)
+)
 
 func (n *wourifsNode) OnAdd(ctx context.Context) {
 	if n.path == "" {
 		return
 	}
-	conn, _ := dialNN(n.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(n.nnAddr)
+	if err != nil {
 		return
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(ctx, &pb.StatFileRequest{Path: n.path})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(rctx, &pb.StatFileRequest{Path: n.path})
 	if err == nil {
 		n.isDir = resp.IsDir
 	}
@@ -51,11 +105,16 @@ func (n *wourifsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOu
 		return nil, syscall.EIO
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(ctx, &pb.StatFileRequest{Path: childPath})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(rctx, &pb.StatFileRequest{Path: childPath})
 	if err != nil {
-		return nil, syscall.ENOENT
+		return nil, mapGRPCErr(err, syscall.ENOENT)
 	}
-	out.Attr = fuse.Attr{Ino: ino(childPath), Size: uint64(resp.SizeBytes), Mode: resp.Mode, Mtime: uint64(resp.MtimeUnix), Ctime: uint64(resp.CtimeUnix)}
+	out.Attr = fuse.Attr{
+		Ino: ino(childPath), Size: uint64(resp.SizeBytes), Mode: resp.Mode,
+		Mtime: uint64(resp.MtimeUnix), Ctime: uint64(resp.CtimeUnix),
+	}
 	child := &wourifsNode{path: childPath, isDir: resp.IsDir, nnAddr: n.nnAddr}
 	if resp.IsDir {
 		out.Attr.Mode |= fuse.S_IFDIR
@@ -71,11 +130,11 @@ func (n *wourifsNode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.A
 		return syscall.EIO
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(ctx, &pb.StatFileRequest{Path: n.path})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).StatFile(rctx, &pb.StatFileRequest{Path: n.path})
 	if err != nil {
-		out.Attr.Mode = fuse.S_IFDIR | 0755
-		out.Attr.Ino = 1
-		return 0
+		return mapGRPCErr(err, syscall.EIO)
 	}
 	mode := resp.Mode
 	if mode == 0 {
@@ -90,7 +149,10 @@ func (n *wourifsNode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.A
 	} else {
 		mode |= fuse.S_IFREG
 	}
-	out.Attr = fuse.Attr{Ino: ino(n.path), Size: uint64(resp.SizeBytes), Mode: mode, Mtime: uint64(resp.MtimeUnix), Ctime: uint64(resp.CtimeUnix)}
+	out.Attr = fuse.Attr{
+		Ino: ino(n.path), Size: uint64(resp.SizeBytes), Mode: mode,
+		Mtime: uint64(resp.MtimeUnix), Ctime: uint64(resp.CtimeUnix),
+	}
 	return 0
 }
 
@@ -103,7 +165,11 @@ func (n *wourifsNode) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.Se
 	client := pb.NewNameNodeServiceClient(conn)
 	if in.Valid&fuse.FATTR_SIZE != 0 {
 		if sz, ok := in.GetSize(); ok {
-			client.TruncateFile(ctx, &pb.TruncateFileRequest{Path: n.path, SizeBytes: int64(sz)})
+			rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+			defer cancel()
+			if _, err := client.TruncateFile(rctx, &pb.TruncateFileRequest{Path: n.path, SizeBytes: int64(sz)}); err != nil {
+				return mapGRPCErr(err, syscall.EIO)
+			}
 		}
 	}
 	return n.Getattr(ctx, fh, out)
@@ -115,11 +181,19 @@ func (n *wourifsNode) Open(ctx context.Context, flags uint32) (fs.FileHandle, ui
 		return nil, 0, syscall.EIO
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).LookupFile(ctx, &pb.LookupFileRequest{Path: n.path})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).LookupFile(rctx, &pb.LookupFileRequest{Path: n.path})
 	if err != nil {
-		return nil, 0, syscall.ENOENT
+		return nil, 0, mapGRPCErr(err, syscall.ENOENT)
 	}
-	return &wourifsFileHandle{path: n.path, fileID: resp.FileId, nnAddr: n.nnAddr, dnConns: map[string]datanodepb.DataNodeServiceClient{}}, fuse.FOPEN_KEEP_CACHE, 0
+	fh := &wourifsFileHandle{
+		path:    n.path,
+		fileID:  resp.FileId,
+		nnAddr:  n.nnAddr,
+		dnConns: make(map[string]*grpc.ClientConn),
+	}
+	return fh, fuse.FOPEN_KEEP_CACHE, 0
 }
 
 func (n *wourifsNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (*fs.Inode, fs.FileHandle, uint32, syscall.Errno) {
@@ -132,14 +206,21 @@ func (n *wourifsNode) Create(ctx context.Context, name string, flags uint32, mod
 		return nil, nil, 0, syscall.EIO
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).CreateFile(ctx, &pb.CreateFileRequest{Path: childPath, Mode: mode})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).CreateFile(rctx, &pb.CreateFileRequest{Path: childPath, Mode: mode})
 	if err != nil {
-		return nil, nil, 0, syscall.EIO
+		return nil, nil, 0, mapGRPCErr(err, syscall.EIO)
 	}
 	child := &wourifsNode{path: childPath, nnAddr: n.nnAddr}
 	out.Attr.Ino = ino(childPath)
 	out.Attr.Mode = fuse.S_IFREG | mode
-	fh := &wourifsFileHandle{path: childPath, fileID: resp.FileId, nnAddr: n.nnAddr, dnConns: map[string]datanodepb.DataNodeServiceClient{}}
+	fh := &wourifsFileHandle{
+		path:    childPath,
+		fileID:  resp.FileId,
+		nnAddr:  n.nnAddr,
+		dnConns: make(map[string]*grpc.ClientConn),
+	}
 	return n.NewInode(ctx, child, fs.StableAttr{Mode: fuse.S_IFREG, Ino: out.Attr.Ino}), fh, 0, 0
 }
 
@@ -153,9 +234,10 @@ func (n *wourifsNode) Mkdir(ctx context.Context, name string, mode uint32, out *
 		return nil, syscall.EIO
 	}
 	defer conn.Close()
-	_, err = pb.NewNameNodeServiceClient(conn).MakeDirectory(ctx, &pb.MakeDirectoryRequest{Path: childPath, Mode: mode})
-	if err != nil {
-		return nil, syscall.EIO
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	if _, err := pb.NewNameNodeServiceClient(conn).MakeDirectory(rctx, &pb.MakeDirectoryRequest{Path: childPath, Mode: mode}); err != nil {
+		return nil, mapGRPCErr(err, syscall.EIO)
 	}
 	child := &wourifsNode{path: childPath, isDir: true, nnAddr: n.nnAddr}
 	out.Attr.Ino = ino(childPath)
@@ -164,29 +246,27 @@ func (n *wourifsNode) Mkdir(ctx context.Context, name string, mode uint32, out *
 }
 
 func (n *wourifsNode) Rmdir(ctx context.Context, name string) syscall.Errno {
-	conn, _ := dialNN(n.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(n.nnAddr)
+	if err != nil {
 		return syscall.EIO
 	}
 	defer conn.Close()
-	_, err := pb.NewNameNodeServiceClient(conn).RemoveDirectory(ctx, &pb.RemoveDirectoryRequest{Path: n.path + "/" + name})
-	if err != nil {
-		return syscall.ENOTEMPTY
-	}
-	return 0
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	_, err = pb.NewNameNodeServiceClient(conn).RemoveDirectory(rctx, &pb.RemoveDirectoryRequest{Path: n.path + "/" + name})
+	return mapGRPCErr(err, syscall.ENOTEMPTY)
 }
 
 func (n *wourifsNode) Unlink(ctx context.Context, name string) syscall.Errno {
-	conn, _ := dialNN(n.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(n.nnAddr)
+	if err != nil {
 		return syscall.EIO
 	}
 	defer conn.Close()
-	_, err := pb.NewNameNodeServiceClient(conn).DeleteFile(ctx, &pb.DeleteFileRequest{Path: n.path + "/" + name})
-	if err != nil {
-		return syscall.ENOENT
-	}
-	return 0
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	_, err = pb.NewNameNodeServiceClient(conn).DeleteFile(rctx, &pb.DeleteFileRequest{Path: n.path + "/" + name})
+	return mapGRPCErr(err, syscall.ENOENT)
 }
 
 func (n *wourifsNode) Rename(ctx context.Context, name string, newParent fs.InodeEmbedder, newName string, flags uint32) syscall.Errno {
@@ -203,27 +283,28 @@ func (n *wourifsNode) Rename(ctx context.Context, name string, newParent fs.Inod
 	if newNode.path == "" {
 		newPath = "/" + newName
 	}
-	conn, _ := dialNN(n.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(n.nnAddr)
+	if err != nil {
 		return syscall.EIO
 	}
 	defer conn.Close()
-	_, err := pb.NewNameNodeServiceClient(conn).RenameFile(ctx, &pb.RenameFileRequest{OldPath: oldPath, NewPath: newPath})
-	if err != nil {
-		return syscall.ENOENT
-	}
-	return 0
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	_, err = pb.NewNameNodeServiceClient(conn).RenameFile(rctx, &pb.RenameFileRequest{OldPath: oldPath, NewPath: newPath})
+	return mapGRPCErr(err, syscall.ENOENT)
 }
 
 func (n *wourifsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
-	conn, _ := dialNN(n.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(n.nnAddr)
+	if err != nil {
 		return nil, syscall.EIO
 	}
 	defer conn.Close()
-	resp, err := pb.NewNameNodeServiceClient(conn).ListDirectory(ctx, &pb.ListDirectoryRequest{Path: n.path})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	resp, err := pb.NewNameNodeServiceClient(conn).ListDirectory(rctx, &pb.ListDirectoryRequest{Path: n.path})
 	if err != nil {
-		return nil, syscall.EIO
+		return nil, mapGRPCErr(err, syscall.EIO)
 	}
 	var entries []fuse.DirEntry
 	for _, e := range resp.Entries {
@@ -255,24 +336,44 @@ type wourifsFileHandle struct {
 	path    string
 	fileID  string
 	nnAddr  string
-	dnConns map[string]datanodepb.DataNodeServiceClient
+	dnConns map[string]*grpc.ClientConn
 	mu      sync.Mutex
 }
 
-var _ fs.FileReader = (*wourifsFileHandle)(nil)
-var _ fs.FileWriter = (*wourifsFileHandle)(nil)
-var _ fs.FileFsyncer = (*wourifsFileHandle)(nil)
-var _ fs.FileFlusher = (*wourifsFileHandle)(nil)
+var (
+	_ fs.FileReader   = (*wourifsFileHandle)(nil)
+	_ fs.FileWriter   = (*wourifsFileHandle)(nil)
+	_ fs.FileFsyncer  = (*wourifsFileHandle)(nil)
+	_ fs.FileFlusher  = (*wourifsFileHandle)(nil)
+	_ fs.FileReleaser = (*wourifsFileHandle)(nil)
+)
+
+// getDNConn returns a cached Datanode connection or creates one.
+func (f *wourifsFileHandle) getDNConn(addr string) (*grpc.ClientConn, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if conn, ok := f.dnConns[addr]; ok {
+		return conn, nil
+	}
+	conn, err := dialNN(addr)
+	if err != nil {
+		return nil, err
+	}
+	f.dnConns[addr] = conn
+	return conn, nil
+}
 
 func (f *wourifsFileHandle) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
-	conn, _ := dialNN(f.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(f.nnAddr)
+	if err != nil {
 		return nil, syscall.EIO
 	}
 	defer conn.Close()
-	lookup, err := pb.NewNameNodeServiceClient(conn).LookupFile(ctx, &pb.LookupFileRequest{Path: f.path})
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	lookup, err := pb.NewNameNodeServiceClient(conn).LookupFile(rctx, &pb.LookupFileRequest{Path: f.path})
 	if err != nil {
-		return nil, syscall.ENOENT
+		return nil, mapGRPCErr(err, syscall.ENOENT)
 	}
 	if len(lookup.Chunks) == 0 {
 		return fuse.ReadResultData([]byte{}), 0
@@ -284,20 +385,25 @@ func (f *wourifsFileHandle) Read(ctx context.Context, dest []byte, off int64) (f
 	}
 	chunk := lookup.Chunks[chunkIdx]
 	offChunk := off % chunkSize
-	dnConn, _ := dialNN(chunk.DatanodeAddress)
-	if dnConn == nil {
+
+	dnConn, err := f.getDNConn(chunk.DatanodeAddress)
+	if err != nil {
 		return nil, syscall.EIO
 	}
-	defer dnConn.Close()
-	stream, _ := datanodepb.NewDataNodeServiceClient(dnConn).ReadChunk(ctx, &datanodepb.ReadChunkRequest{ChunkId: chunk.ChunkId})
-	if stream == nil {
-		return nil, syscall.EIO
+	rctx2, cancel2 := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel2()
+	stream, err := datanodepb.NewDataNodeServiceClient(dnConn).ReadChunk(rctx2, &datanodepb.ReadChunkRequest{ChunkId: chunk.ChunkId})
+	if err != nil {
+		return nil, mapGRPCErr(err, syscall.EIO)
 	}
 	var data []byte
 	for {
-		resp, err := stream.Recv()
-		if err != nil {
+		resp, ioErr := stream.Recv()
+		if ioErr == io.EOF {
 			break
+		}
+		if ioErr != nil {
+			return nil, mapGRPCErr(ioErr, syscall.EIO)
 		}
 		data = append(data, resp.Data...)
 		if resp.IsLast {
@@ -315,48 +421,49 @@ func (f *wourifsFileHandle) Read(ctx context.Context, dest []byte, off int64) (f
 }
 
 func (f *wourifsFileHandle) Write(ctx context.Context, data []byte, off int64) (uint32, syscall.Errno) {
-	conn, _ := dialNN(f.nnAddr)
-	if conn == nil {
+	conn, err := dialNN(f.nnAddr)
+	if err != nil {
 		return 0, syscall.EIO
 	}
 	defer conn.Close()
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
 	client := pb.NewNameNodeServiceClient(conn)
 	chunkIdx := int32(off / (64 << 20))
-	alloc, err := client.AllocateChunk(ctx, &pb.AllocateChunkRequest{FileId: f.fileID, ChunkIndex: chunkIdx, ReplicationFactor: 3})
+	alloc, err := client.AllocateChunk(rctx, &pb.AllocateChunkRequest{FileId: f.fileID, ChunkIndex: chunkIdx, ReplicationFactor: 3})
 	if err != nil {
-		return 0, syscall.EIO
+		return 0, mapGRPCErr(err, syscall.EIO)
 	}
 	if len(alloc.DatanodeAddresses) == 0 {
 		return 0, syscall.EIO
 	}
-	dnConn, _ := dialNN(alloc.DatanodeAddresses[0])
-	if dnConn == nil {
-		return 0, syscall.EIO
-	}
-	defer dnConn.Close()
-	wstream, _ := datanodepb.NewDataNodeServiceClient(dnConn).WriteChunk(ctx)
-	if wstream == nil {
-		return 0, syscall.EIO
-	}
-	wstream.Send(&datanodepb.WriteChunkRequest{ChunkId: alloc.ChunkId, Data: data, BlockIndex: 0, IsLast: true})
-	_, err = wstream.CloseAndRecv()
+	dnConn, err := f.getDNConn(alloc.DatanodeAddresses[0])
 	if err != nil {
 		return 0, syscall.EIO
+	}
+	rctx2, cancel2 := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel2()
+	wstream, err := datanodepb.NewDataNodeServiceClient(dnConn).WriteChunk(rctx2)
+	if err != nil {
+		return 0, mapGRPCErr(err, syscall.EIO)
+	}
+	wstream.Send(&datanodepb.WriteChunkRequest{ChunkId: alloc.ChunkId, Data: data, BlockIndex: 0, IsLast: true})
+	if _, err = wstream.CloseAndRecv(); err != nil {
+		return 0, mapGRPCErr(err, syscall.EIO)
 	}
 	return uint32(len(data)), 0
 }
 
 func (f *wourifsFileHandle) Fsync(ctx context.Context, flags uint32) syscall.Errno {
-	conn, _ := dialNN(f.nnAddr)
-	if conn == nil {
-		return syscall.EIO
-	}
-	defer conn.Close()
-	_, err := pb.NewNameNodeServiceClient(conn).StatFile(ctx, &pb.StatFileRequest{Path: f.path})
+	conn, err := dialNN(f.nnAddr)
 	if err != nil {
 		return syscall.EIO
 	}
-	return 0
+	defer conn.Close()
+	rctx, cancel := context.WithTimeout(ctx, rpcTimeout)
+	defer cancel()
+	_, err = pb.NewNameNodeServiceClient(conn).StatFile(rctx, &pb.StatFileRequest{Path: f.path})
+	return mapGRPCErr(err, syscall.EIO)
 }
 
 func (f *wourifsFileHandle) Flush(ctx context.Context) syscall.Errno { return 0 }
@@ -365,17 +472,13 @@ func (f *wourifsFileHandle) Release(ctx context.Context) syscall.Errno {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 	for _, c := range f.dnConns {
-		c.(interface{ Close() error }).Close()
+		c.Close()
 	}
 	f.dnConns = nil
 	return 0
 }
 
-// --- helpers ---
-
-func dialNN(addr string) (*grpc.ClientConn, error) {
-	return grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-}
+// --- shared helpers ---
 
 func ino(path string) uint64 {
 	var h uint64
@@ -387,3 +490,6 @@ func ino(path string) uint64 {
 	}
 	return h
 }
+
+// keep imports alive
+var _ = fmt.Sprintf
